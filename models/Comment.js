113332c18421acd233f5188f69b9599bdfcb20ca
unknown <arumansalik45@gmail.com>
2025-09-13 22:00:37 +0530

    Comment Model Added

---
+++ models/Comment.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+const commentSchema = new mongoose.Schema({
+    content: { type: String, required: true },
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    post: { type:mongoose.Schema.Types.ObjectId, ref: "Post", required: true },
+    parentComment: { type: mongoose.Schema.Types.ObjectId, ref: 'Comment', required: true },
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }]
+}, { timestamps: true });
+
+export default mongoose.model('Comment', commentSchema);
