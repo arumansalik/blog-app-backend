716b53864cf74cde1417418e44208abaa50811eb
unknown <arumansalik45@gmail.com>
2025-09-15 10:13:28 +0530

    Update a Post Route Added

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -50,3 +50,26 @@ router.get("/:id", async (req, res) => {
         res.status(500).json({message: err.message});
     }
 });
+
+router.put("/:id", protect, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if(!post) return res.status(404).json({ message: "Post Not Found" });
+
+        if(post.author.toString() !== req.user._id.toString()) {
+            return res.status(401).json({ message: "Not authorized" });
+        }
+
+        post.title = req.body.title || post.title;
+        post.content = req.body.content || post.content;
+        post.tags = req.body.tags || post.tags;
+        post.cover = req.body.cover || post.cover;
+
+        const updatedPost = await post.save();
+        res.json(updatedPost);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
