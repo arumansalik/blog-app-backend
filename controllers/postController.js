b36ceebeab82a0888f970529032befaa268a2654
unknown <arumansalik45@gmail.com>
2025-09-16 09:38:55 +0530

    Increment Views

--- controllers/postController.js
+++ controllers/postController.js
@@ -40,3 +40,21 @@ export const toggleBookmarkPost = async (req, res) => {
         res.status(500).json({ message: e.message });
     }
 }
+
+export const incrementPostView = async (req, res) => {
+    try {
+        const { postId } = req.params;
+
+        const post = await Post.findByIdAndUpdate(
+            postId,
+            {  $inc: { views: 1 } },
+            { new: true}
+        );
+
+        if(!post) return res.status(404).json({ message: 'Post not found!' });
+
+        res.json(post);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
