289fa525bf850bafaa4b031b12c747aa9f0029be
unknown <arumansalik45@gmail.com>
2025-09-16 09:32:47 +0530

    Bookmark / Unbookmark a Post

--- controllers/postController.js
+++ controllers/postController.js
@@ -20,3 +20,23 @@ export const toggleLikePost = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
+
+export const toggleBookmarkPost = async (req, res) => {
+    try {
+        const { postId } = req.params;
+        const userId = req.user._id;
+
+        const post = await Post.findById(postId);
+        if(!post) return res.status(404).json({ message: 'Post not found!' });
+
+        if(post.bookmarks.includes(userId)) {
+            post.bookmarks.pull(userId);
+        } else {
+            post.bookmarks.push(userId);
+        }
+
+        await post.save();
+    } catch (e) {
+        res.status(500).json({ message: e.message });
+    }
+}
