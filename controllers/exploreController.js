bfeecf483c6ee7d36cd890266aa9bc15d16e50ca
unknown <arumansalik45@gmail.com>
2025-09-17 23:50:37 +0530

    get Trending Posts

---
+++ controllers/exploreController.js
@@ -0,0 +1,14 @@
+import Post from "../models/Post.js";
+
+export const getTrendingPosts = async (req, res) => {
+    try {
+        const posts = await Post.find()
+            .populate("author", "username avatar")
+            .sort({likes: -1, views: -1})
+            .limit(10);
+
+        res.json(posts);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
